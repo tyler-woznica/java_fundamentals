c2a0467862cba9549669408320b710c43769382b
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-20 10:51:13 -0700

    completed exercise and added printing values

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,16 +14,20 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
-
+        int var1 = str.length();
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
-
+        boolean strcheck = (str == str2);
         // please concatenate str & str2 and set the result to a new String variable below
-
+        String str3 = (str + str2);
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
-
+        boolean strcontain = str.contains("!");
+        System.out.println("var1 contains the length of str: " + var1);
+        System.out.println("strcheck checks if str is equal to str2: " + strcheck);
+        System.out.println("str3 combines str and str2: " + str3);
+        System.out.println("strcontain checks if str contains an exclamation point: " + strcontain);
     }
-
+        
 
 }
