f599012569a91905ae763e5eb97226d90e7e998b
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-20 10:30:04 -0700

    add primitive data types and print values

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -13,9 +13,25 @@ public class Exercise_04 {
     public static void main(String[] args) {
 
         int i = 1;
-        System.out.println("int i is: " + i);
+
+        System.out.println("int i is: " + i );
 
         // write your code below
+
+        boolean b = true;
+        char c = 't';
+        byte byt = 7;
+        short s = 11;
+        long l = 1024;
+        float f = 1024.42f;
+        double d = 2048.8402;
+        System.out.println("boolean b is: " + b);
+        System.out.println("char c is: " + c);
+        System.out.println("byte byt is: " + byt);
+        System.out.println("short s is: " + s );
+        System.out.println("long l is: " + l );
+        System.out.println("float f is: " + f);
+        System.out.println("double d is: " + d);
 
     }
 
