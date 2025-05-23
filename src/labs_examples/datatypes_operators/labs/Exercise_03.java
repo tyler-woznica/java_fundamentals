643bc15afbdcf8ab3039d3d877dd1e138520f177
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-23 07:56:38 -0700

    completed Exercise_03 for datatypes_operators

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,7 +11,18 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
-
+        int x = 10;
+        int y = 50;
+        int z = 15;
+        int add= x + y;
+        int subtract = x - y;
+        int multiply = x * y;
+        int divide = y / x;
+        int modulus = z % x;
+        System.out.println("x=" + x + ", y=" + y + ", and z=" + z);
+        System.out.println("x+y and x-y equals: " + add + ", and " +subtract);
+        System.out.println("x*y and y/x equals: " + multiply + ", and " + divide);
+        System.out.println("x%y equals " + modulus);
     }
 
 }
