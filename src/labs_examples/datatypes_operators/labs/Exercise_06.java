de6e3a4788846cfc3b936a1dca19d2e473b15f38
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-23 08:35:33 -0700

    completed Exercise_06 for datatypes_operators

--- src/labs_examples/datatypes_operators/labs/Exercise_06.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_06.java
@@ -13,6 +13,12 @@ public class Exercise_06 {
     public static void main(String[] args) {
 
         // write code here
-
+        float radius = 3.14f;
+        float height = 5.0f;
+        float pi = 3.14159f;
+        float volume = pi * radius * radius * height;
+        float surfaceArea = (2 * pi * radius * radius) + (2 * pi * radius * height);
+        System.out.println("The volume of a cylinder with a radius of 3.14 and a heigh of 5 is: " + volume);
+        System.out.println("The surface area of that same cylinder is: " + surfaceArea);
     }
 }
