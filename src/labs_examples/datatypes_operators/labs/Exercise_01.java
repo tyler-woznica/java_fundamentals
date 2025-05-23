e27e2482ca3916969119c4764833412cd42b2be8
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-22 18:21:15 -0700

    completed Exercise_01 for datatypes_operators

--- src/labs_examples/datatypes_operators/labs/Exercise_01.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_01.java
@@ -17,7 +17,12 @@ public class Exercise_01 {
     public static void main(String[] args) {
 
         // write code here
+        int i = 10;
+        double d = 1000.0001;
+        float f = 10.01f;
+        char c = 't';
 
+        System.out.println("The values of i, d, f, and c are:\n" + i + "\n" + d + "\n" + f + "\n" + c);
     }
 }
 
