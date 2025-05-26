8b496317a9bea48ab4ef1e64fd63fa35c2649495
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-26 09:06:57 -0700

    grammar and bracket positioning

--- src/labs_examples/conditions_loops/examples/Example4.java
+++ src/labs_examples/conditions_loops/examples/Example4.java
@@ -2,13 +2,13 @@ package labs_examples.conditions_loops.examples;
 
 // Guess the letter game, 3rd version.
 class Guess3 {
-    public static void main(String args[])
+    public static void main(String[] args)
             throws java.io.IOException {
 
         char ch, answer = 'K';
 
         System.out.println("I'm thinking of a letter between A and Z.");
-        System.out.print("Can you guess it: ");
+        System.out.print("Can you guess it?: ");
 
         ch = (char) System.in.read(); // get a char
 
