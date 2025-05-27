f71ba2c303fdd3f6f3d42959abf3b586b46a68fd
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-27 15:15:57 -0700

    completed Exercise_11 of conditions_loops

--- src/labs_examples/conditions_loops/labs/Exercise_11.java
+++ src/labs_examples/conditions_loops/labs/Exercise_11.java
@@ -22,5 +22,12 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_11 {
-
+    public static void main(String[] args) {
+        for (int x = 1; x <= 10; x++) {
+            for (int y = 1; y <= 10; y++) {
+                System.out.print(x * y + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
