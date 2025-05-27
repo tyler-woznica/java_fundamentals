45bc39c6fc5f38d7387520e8fadfe2cbd570ea0f
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-27 05:48:36 -0700

    completed Exercise_02 for conditions_loops

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -14,8 +16,32 @@ package labs_examples.conditions_loops.labs;
 public class Exercise_02 {
 
     public static void main(String[] args) {
-    
-        // write completed here
 
+        // write completed here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number from 1 to 7: ");
+        int daynum = scanner.nextInt();
+
+        if (daynum >= 1 & daynum  <= 7) {
+            if (daynum == 1) {
+                System.out.println("1 corresponds to Sunday!");
+            } else if (daynum == 2) {
+                System.out.println("2 corresponds to Monday!");
+            } else if (daynum == 3) {
+                System.out.println("3 corresponds to Tuesday!");
+            } else if (daynum == 4) {
+                System.out.println("4 corresponds to Wednesday!");
+            } else if (daynum == 5) {
+                System.out.println("5 corresponds to Thursday!");
+            } else if (daynum == 6) {
+                System.out.println("6 corresponds to Friday!");
+            } else if (daynum == 7) {
+                System.out.println("7 corresponds to Saturday!");
+            } 
+        }
+
+        if (daynum < 1 | daynum > 7) {
+            System.out.println("That number is out of range.");
+        }
     }
 }
