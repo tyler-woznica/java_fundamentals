cc5f112f8548f31011a44344c97f081a853b8e51
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-29 20:11:52 -0700

    moved bracket in main method to standar location

--- src/labs_examples/arrays/examples/Example3.java
+++ src/labs_examples/arrays/examples/Example3.java
@@ -2,7 +2,7 @@ package labs_examples.arrays.examples;
 
 // Use array initializers.
 class MinMax2 {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int nums[] = { 99, -10, 100123, 18, -978,
                 5623, 463, -9, 287, 49 };
 
