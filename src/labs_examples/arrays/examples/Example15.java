c55a44c94418fca66397c811e0925ea8c194e2e4
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-31 14:59:16 -0700

    added space to arithmetic for readability

--- src/labs_examples/arrays/examples/Example15.java
+++ src/labs_examples/arrays/examples/Example15.java
@@ -8,8 +8,8 @@ class ForEach2 {
 
         // give nums some values
         for(int i = 0; i < 3; i++)
-            for(int j=0; j < 5; j++)
-                nums[i][j] = (i+1)*(j+1);
+            for(int j = 0; j < 5; j++)
+                nums[i][j] = (i + 1) * (j + 1);
 
         // Use for-each for loop to display and sum the values.
         for(int[] array : nums) {
