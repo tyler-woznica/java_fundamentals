a09d281dc7d055a26695c7a21dc97aea2beb3337
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-31 12:54:58 -0700

    adjusted spacing in nested for loop on line 20

--- src/labs_examples/arrays/examples/Example8.java
+++ src/labs_examples/arrays/examples/Example8.java
@@ -17,8 +17,8 @@ class Squares {
         };
         int i, j;
 
-        for(i=0; i < 10; i++) {
-            for(j=0; j < 2; j++)
+        for(i = 0; i < 10; i++) {
+            for(j = 0; j < 2; j++)
                 System.out.print(sqrs[i][j] + " ");
             System.out.println();
         }
