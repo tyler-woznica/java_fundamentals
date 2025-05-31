3024f8e547548187833715cd844b43c21d568999
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-31 13:20:38 -0700

    Added comments and adjusted spacing in for loops

--- src/labs_examples/arrays/examples/Example11.java
+++ src/labs_examples/arrays/examples/Example11.java
@@ -2,12 +2,12 @@ package labs_examples.arrays.examples;
 
 // Use length variable to help copy an array.
 class ACopy {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Ty: moved brackets to String
         int i;
-        int[] nums1 = new int[10];
-        int[] nums2 = new int[10];
+        int[] nums1 = new int[10]; // Ty: moved brackets for nums1
+        int[] nums2 = new int[10]; // Ty: moved brackets for nums2
 
-        for(i=0; i < nums1.length; i++)
+        for(i = 0; i < nums1.length; i++)
             nums1[i] = i;
 
         // copy nums1 to nums2
@@ -15,7 +15,7 @@ class ACopy {
             for(i = 0; i < nums2.length; i++)
                 nums2[i] = nums1[i];
 
-        for(i=0; i < nums2.length; i++)
+        for(i = 0; i < nums2.length; i++)
             System.out.print(nums2[i] + " ");
     }
 }
