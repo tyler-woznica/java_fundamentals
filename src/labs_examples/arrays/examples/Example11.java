1d955e6ad6684bfe648b334af5e3c63a833f535b
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-31 13:24:30 -0700

    removed if statement as it was always true

--- src/labs_examples/arrays/examples/Example11.java
+++ src/labs_examples/arrays/examples/Example11.java
@@ -11,9 +11,9 @@ class ACopy {
             nums1[i] = i;
 
         // copy nums1 to nums2
-        if(nums2.length >= nums1.length)
-            for(i = 0; i < nums2.length; i++)
-                nums2[i] = nums1[i];
+        // Ty: removed if as nums2.length >= nums1.length always true
+        for(i = 0; i < nums2.length; i++)
+            nums2[i] = nums1[i];
 
         for(i = 0; i < nums2.length; i++)
             System.out.print(nums2[i] + " ");
