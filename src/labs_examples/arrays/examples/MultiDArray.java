93bc665785b38a0130c2eff79eb5a8ace1f867b3
Tyler Woznica <tylerwoznica@icloud.com>
2025-05-31 17:23:07 -0700

    moved brackets for main and made arithmetic more readable

--- src/labs_examples/arrays/examples/MultiDArray.java
+++ src/labs_examples/arrays/examples/MultiDArray.java
@@ -3,22 +3,22 @@ package labs_examples.arrays.examples;
 //for more 2D array examples see: labs_examples.datatypes_operators/examples/labs_examples.arrays
 
 class MultiDArray {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int t, i;
         // declare and initialize array
         int[][] table = new int[3][4];
 
         // populate 2D array
-        for(t=0; t < 3; t++) {
+        for(t = 0; t < 3; t++) {
 
-            for(i=0; i < 4; i++) {
+            for(i = 0; i < 4; i++) {
                 table[t][i] = (t*4)+i+1;
             }
         }
 
         // print out each element in 2D array
-        for(t=0; t < 3; t++) {
-            for(i=0; i < 4; i++) {
+        for(t = 0; t < 3; t++) {
+            for(i = 0; i < 4; i++) {
                 System.out.print(table[t][i] + " ");
             }
             System.out.println();
