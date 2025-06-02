5e98fb4eef4fc59f7128010340dd1daf04192827
Tyler Woznica <tylerwoznica@icloud.com>
2025-06-01 20:05:35 -0700

    completed Exercise_01

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,6 +1,7 @@
 package labs_examples.arrays.labs;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Arrays calculator
@@ -13,11 +14,25 @@ import java.util.Arrays;
  */
 
 public class Exercise_01 {
+    public static void main(String[] args) {
 
-    int[] nums = new [10];
-    //need to take in user input for 10 numbers
-    int sum;
-    float avg;
-    //for (int i : nums)
+        Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        float avg;
+        int[] nums = new int[10];
+
+        System.out.println("After entering 10 numbers, you will get the sum and the average.");
+
+        for (int i = 0; i < 10; i++) { // populate nums and calculate sums from input
+            System.out.print("Please enter number " + (i + 1) + ": ");
+            nums[i] = scanner.nextInt();
+            sum += nums[i];
+        }
+
+        avg = ((float)sum / nums.length);
+
+        System.out.println("The sum is: " + sum);
+        System.out.println("The average is: " + avg);
+    }
 }
