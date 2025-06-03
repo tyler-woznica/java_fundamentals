4b25cf1ba7e2a85e36a5aba931861ef99417d1b6
Tyler Woznica <tylerwoznica@icloud.com>
2025-06-02 21:15:39 -0700

    complete Exercise_04

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -10,4 +10,25 @@ package labs_examples.arrays.labs;
 
 public class Exercise_04 {
 
+    public static void main(String[] args) {
+
+        int[][] irregular = new int[4][];
+        irregular[0] = new int[2];
+        irregular[1] = new int[3];
+        irregular[2] = new int[4];
+        irregular[3] = new int[5];
+
+        for (int i = 0; i < irregular.length; i++) {
+            for (int j = 0; j < irregular[i].length; j++) {
+                irregular[i][j] = (i + 1) * j;
+            }
+        }
+
+        for (int[] int1 : irregular) {
+            for (int int2 : int1) {
+                System.out.print(int2 + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
