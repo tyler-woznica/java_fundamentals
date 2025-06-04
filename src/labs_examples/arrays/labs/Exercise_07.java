651ebeecba431e8e9891057be553efb6a3d2dff3
Tyler Woznica <tylerwoznica@icloud.com>
2025-06-03 19:27:52 -0700

    completed Exercise_07

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.ArrayList;
+
 /**
  *  ArrayLists
  *
@@ -10,4 +12,38 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Alice");
+        names.add("Brandon");
+        names.add("Charlie");
+        names.add("Deborah");
+        names.add("Eric");
+
+        System.out.println(names.get(0));
+
+        names.set(1, "Branson");
+        System.out.println(names.get(1));
+
+        names.add("Francis");
+
+        names.remove("Charlie");
+        System.out.println(names.get(2));
+
+        int var = names.size();
+        System.out.println("The Array list contains " + var + " names.");
+
+        if (names.contains("Francis")) {
+            System.out.println("Francis is on the list.");
+        }
+
+        names.clear();
+        if (names.isEmpty()) {
+            System.out.println("There are no more names in the list!");
+        } else {
+            System.out.println("There are still names in the list.");
+        }
+    }
 }
