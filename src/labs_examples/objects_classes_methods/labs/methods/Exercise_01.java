1cf50299bb7ed1f74926e30dc611269fceae26e9
Tyler Woznica <tylerwoznica@icloud.com>
2025-06-11 06:54:50 -0700

    completed Exercise_01

--- src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
@@ -10,8 +10,12 @@ public class Exercise_01 {
         // please create the methods as instructed below then
         // call each of those methods from here, within the main()
 
-    }
+        System.out.println("10 * 5 = " + multiply(10, 5));
+        System.out.println("10 / 5 = " + divide(10, 5));
+        joke();
+        secYear(2);
 
+    }
 
     // 1) Create a static multiply() method below that takes two int arguments (int a, int b) and
     //    returns the result of a * b
@@ -23,21 +27,24 @@ public class Exercise_01 {
     // 2) Create a static divide() method below that takes two int arguments (int a, int b) and
     //    returns the result of a / b
     public static int divide(int a, int b) {
-
+        return a / b;
     }
 
     // 3) Create a static void method that will print of joke of your choice to the console
-
+    public static void joke() {
+        System.out.println("What did the priest say to the assembly of vegetables?");
+        System.out.println("Lettuce pray.");
+    }
 
     // 4) Create a static method that takes in a number in years (int years) as an argument
     //    and returns the number of seconds that number in years represents
-
+    public static void secYear(int a) {
+        int secs = a * 31536000;
+        System.out.println("There are " + secs + " in " + a + " year(s).");
+    }
 
     // 5) Create a varargs method that will return the length of the varargs array passed in
-
-
-
-
-
-
+    static int countArgs(int... numbers) {
+        return numbers.length;
+    }
 }
