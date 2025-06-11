90b493b6575cd52126d94e44a34e0e555f947f0c
Tyler Woznica <tylerwoznica@icloud.com>
2025-06-10 21:14:31 -0700

    added multiply and divide method yet incomplete

--- src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
@@ -16,10 +16,15 @@ public class Exercise_01 {
     // 1) Create a static multiply() method below that takes two int arguments (int a, int b) and
     //    returns the result of a * b
 
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
 
     // 2) Create a static divide() method below that takes two int arguments (int a, int b) and
     //    returns the result of a / b
+    public static int divide(int a, int b) {
 
+    }
 
     // 3) Create a static void method that will print of joke of your choice to the console
 
