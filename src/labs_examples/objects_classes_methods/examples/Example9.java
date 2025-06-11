00c0fa748c0c8e96c69ff0411e6ad5c4fe2b5627
Tyler Woznica <tylerwoznica@icloud.com>
2025-06-10 17:39:00 -0700

    simplified if statement for isValid

--- src/labs_examples/objects_classes_methods/examples/Example9.java
+++ src/labs_examples/objects_classes_methods/examples/Example9.java
@@ -57,10 +57,7 @@ class Help {
     }
 
     boolean isValid(int ch) {
-        if(ch < '1' | ch > '7' & ch != 'q')
-            return false;
-        else
-            return true;
+        return !(ch < '1' | ch > '7' & ch != 'q');
     }
 
 }
