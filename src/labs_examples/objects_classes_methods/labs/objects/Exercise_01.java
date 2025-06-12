ff2b76641eb22c8ef56b20b34f97ff17e14186cd
Tyler Woznica <tylerwoznica@icloud.com>
2025-06-11 19:17:59 -0700

    started structure of Ex1

---
+++ src/labs_examples/objects_classes_methods/labs/objects/Exercise_01.java
@@ -0,0 +1,47 @@
+package labs_examples.objects_classes_methods.labs.objects;
+
+public class Exercise_01 {
+
+    public static void main(String[] args) {
+
+    }
+
+class Engine {
+
+}
+
+class Wings {
+
+}
+
+class Passengers {
+
+}
+
+class Crew {
+
+}
+
+class Airplane {
+    double fuelcapacity;
+    double currentfuel;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
