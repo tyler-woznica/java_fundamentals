549d08bae0839a562531d241ceb41c4b34543ef9
Tyler Woznica <tylerwoznica@icloud.com>
2025-06-11 20:19:25 -0700

    added varialbes and expanded classes

--- src/labs_examples/objects_classes_methods/labs/objects/Exercise_01.java
+++ src/labs_examples/objects_classes_methods/labs/objects/Exercise_01.java
@@ -7,19 +7,38 @@ public class Exercise_01 {
     }
 
 class Engine {
-
+    String brand;
+    int quantity;
+    String type;
 }
 
 class Wings {
-
+    int span;
+    boolean painted;
+    public Wings (int span, boolean painted) {
+        this.span = span;
+        this.painted = painted;
+    }
 }
 
 class Passengers {
-
+    int seats;
+    boolean screens;
+    boolean power;
+    public Passengers (int seats, boolean screens, boolean power) {
+        this.seats = seats;
+        this.screens = screens;
+        this.power = power;
+    }
 }
 
 class Crew {
-
+    int crewcount;
+    String pilot;
+    public Crew (int crewcount, String pilot) {
+        this.crewcount = crewcount;
+        this.pilot = pilot;
+    }
 }
 
 class Airplane {
