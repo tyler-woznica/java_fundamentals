cbfc791eafb3effb42bfff05e9a6ed40e1992d1f
Tyler Woznica <tylerwoznica@icloud.com>
2025-06-12 21:28:11 -0700

    created and completed Exercise_04

---
+++ src/labs_examples/objects_classes_methods/labs/objects/ex04/Exercise_04.java
@@ -0,0 +1,15 @@
+package labs_examples.objects_classes_methods.labs.objects.ex04;
+
+//In the main() method, create at least 3 objects of your POJO class, each using a different constructor.
+public class Exercise_04 {
+    public static void main(String[] args) {
+
+        Pojo pojo1 = new Pojo(1, 2, 2.2);
+        Pojo pojo2 = new Pojo(1, 2);
+        Pojo pojo3 = new Pojo(1);
+
+        System.out.println(pojo1.x + ", " + pojo1.y + ", " + pojo1.z);
+        System.out.println(pojo2.x + ", " + pojo2.y + ", " + pojo2.z);
+        System.out.println(pojo3.x + ", " + pojo3.y + ", " + pojo3.z);
+    }
+}
---
+++ src/labs_examples/objects_classes_methods/labs/objects/ex04/Pojo.java
@@ -0,0 +1,23 @@
+package labs_examples.objects_classes_methods.labs.objects.ex04;
+
+//In this POJO, demonstrate at least three overloaded constructors.
+public class Pojo {
+    public int x;
+    public int y;
+    public double z;
+
+    public Pojo(int x, int y, double b) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Pojo(int x) {
+        this.x = x;
+    }
+
+    public Pojo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
