47594188b2fe3f55f1af32c4f811316526c6af89
Tyler Woznica <tylerwoznica@icloud.com>
2025-06-18 11:02:42 -0500

    created getters and setters

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_01_package/FuelType.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_01_package/FuelType.java
@@ -6,5 +6,43 @@ public class FuelType {
     int fuelcost;
     boolean fuelcooling;
 
+    public FuelType(String fueltype, int fuelcost, boolean fuelcooling) {
+        this.fueltype = fueltype;
+        this.fuelcost = fuelcost;
+        this.fuelcooling = fuelcooling;
+    }
+
+    public String getFueltype() {
+        return fueltype;
+    }
+
+    public void setFueltype(String fueltype) {
+        this.fueltype = fueltype;
+    }
+
+    public int getFuelcost() {
+        return fuelcost;
+    }
+
+    public void setFuelcost(int fuelcost) {
+        this.fuelcost = fuelcost;
+    }
+
+    public boolean isFuelcooling() {
+        return fuelcooling;
+    }
+
+    public void setFuelcooling(boolean fuelcooling) {
+        this.fuelcooling = fuelcooling;
+    }
+
+    @Override
+    public String toString() {
+        return "FuelType{" +
+                "fueltype='" + fueltype + '\'' +
+                ", fuelcost=" + fuelcost +
+                ", fuelcooling=" + fuelcooling +
+                '}';
+    }
 
 }
--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_01_package/Generator.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_01_package/Generator.java
@@ -7,4 +7,54 @@ public class Generator {
     String cooling;
     int gencost;
 
+    public Generator(double output, String manufacturer, String cooling, int gencost) {
+        this.output = output;
+        this.manufacturer = manufacturer;
+        this.cooling = cooling;
+        this.gencost = gencost;
+        
+    }
+
+    public double getOutput() {
+        return output;
+    }
+
+    public void setOutput(double output) {
+        this.output = output;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getCooling() {
+        return cooling;
+    }
+
+    public void setCooling(String cooling) {
+        this.cooling = cooling;
+    }
+
+    public int getGencost() {
+        return gencost;
+    }
+
+    public void setGencost(int gencost) {
+        this.gencost = gencost;
+    }
+
+    @Override
+    public String toString() {
+        return "Generator{" +
+                "output=" + output +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", cooling='" + cooling + '\'' +
+                ", gencost=" + gencost +
+                '}';
+    }
+
 }
