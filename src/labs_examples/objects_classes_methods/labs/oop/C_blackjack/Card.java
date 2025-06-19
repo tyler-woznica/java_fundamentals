a769d88087716affdf5ee2d89627ddc9e45b7b70
Tyler Woznica <tylerwoznica@icloud.com>
2025-06-19 13:31:54 -0500

    created classes and initial field

---
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Card.java
@@ -0,0 +1,7 @@
+package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
+
+public class Card {
+
+    static char[] suit = new char[]{'♠', '♦', '♥', '♣'};
+    int cardValue;
+}
---
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Deck.java
@@ -0,0 +1,12 @@
+package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
+
+import java.util.ArrayList;
+
+public class Deck {
+
+    Card[] cards;
+    ArrayList<Integer> usedCards;
+    // private void populateDeck () {}
+
+    
+}
