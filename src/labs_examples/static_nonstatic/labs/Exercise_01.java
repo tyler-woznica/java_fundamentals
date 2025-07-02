e93048a8474d59a28a0682ce23a956c791b588b7
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-02 07:50:48 -0700

    began exercise 1. created structure

---
+++ src/labs_examples/static_nonstatic/labs/Exercise_01.java
@@ -0,0 +1,30 @@
+package labs_examples.static_nonstatic.labs;
+
+public class Exercise_01 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    static class Stactial1 {
+
+        static void statical2 () {
+
+        }
+        void statical3 () {
+
+        }
+    }
+
+    public class nonStatical1 {
+        static void nonstatical2() {
+
+        }
+        void nonstatical3() {
+
+        }
+    }
+}
+
