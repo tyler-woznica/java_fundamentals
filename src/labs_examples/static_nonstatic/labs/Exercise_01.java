88350d9a6e51ab594a2128fca3968f71ff548fef
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-02 10:33:57 -0700

    added methods. still testing

--- src/labs_examples/static_nonstatic/labs/Exercise_01.java
+++ src/labs_examples/static_nonstatic/labs/Exercise_01.java
@@ -4,26 +4,34 @@ public class Exercise_01 {
 
     public static void main(String[] args) {
 
+        Stactial1.statical2();
+
     }
 
 
 
-    static class Stactial1 {
+    public static class Stactial1 {
 
-        static void statical2 () {
-
+        public static void statical2 () {
+            statical3();
+            Stactial1 obj = new Stactial1;
+            statical4("statical4");
         }
-        void statical3 () {
-
+        public static void statical3() {
+            System.out.println("I'm statical 3");
+        }
+        public void statical4(String text) {
+            System.out.println(text);
         }
     }
 
     public class nonStatical1 {
-        static void nonstatical2() {
+        public static void nonstatical2() {
 
         }
-        void nonstatical3() {
-
+        public int nonstatical3(int a, int b) {
+            int sum = a + b;
+            return sum;
         }
     }
 }
