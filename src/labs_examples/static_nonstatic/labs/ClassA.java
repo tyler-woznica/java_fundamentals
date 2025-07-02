7574d29ef77013e5e96cd1d9e17419fd6de64f22
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-02 14:05:36 -0700

    completed exercise1

--- src/labs_examples/static_nonstatic/labs/ClassA.java
+++ src/labs_examples/static_nonstatic/labs/ClassA.java
@@ -3,11 +3,31 @@ package labs_examples.static_nonstatic.labs;
 public class ClassA {
 
     public static void main(String[] args) {
+        staticMethod();
+        ClassB.staticMethodB();
+        ClassA obj = new ClassA();
+        obj.nonStatic();
+        ClassB obj1 = new ClassB();
+        obj1.nonStaticB();
 
     }
 
     public static void staticMethod () {
 
+    }
+
+    public void nonStatic2() {
+        nonStatic();
+        ClassB obj = new ClassB();
+        obj.nonStaticB();
+        staticMethod();
+        ClassB.staticMethodB();
+        double py = Math.PI;
+
+    }
+
+    public void nonStatic() {
+        System.out.println("non static method");
     }
 
 }
--- src/labs_examples/static_nonstatic/labs/ClassB.java
+++ src/labs_examples/static_nonstatic/labs/ClassB.java
@@ -2,6 +2,13 @@ package labs_examples.static_nonstatic.labs;
 
 public class ClassB {
 
+    public static void staticMethodB () {
+
+    }
+
+    public void nonStaticB() {
+        System.out.println("nonstatic B");
+    }
 
 
 }
