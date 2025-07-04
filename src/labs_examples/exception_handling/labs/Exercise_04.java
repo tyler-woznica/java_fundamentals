9cb9d75ad9ade105a73ec1b07d964f87f5c52456
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-04 11:32:25 -0700

    completed exercise 4

--- src/labs_examples/exception_handling/labs/Exercise_04.java
+++ src/labs_examples/exception_handling/labs/Exercise_04.java
@@ -6,3 +6,33 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a nested try/catch.
  *
  */
+
+public class Exercise_04 {
+
+    public static void main(String[] args) {
+
+        try {
+
+            try {
+                someMethod1(4, 0);
+            } catch (ArithmeticException e) {
+                System.out.println("Cannot divide by zero.");
+            }
+
+            someMethod2(5);
+        }   catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Index provided is out of bounds.");
+        }
+
+    }
+
+    public static int someMethod1(int a, int b) throws ArithmeticException{
+        return a/ b;
+    }
+
+    public static int someMethod2(int a) throws ArrayIndexOutOfBoundsException {
+        int[] nums = {1, 2, 3};
+        return nums[a];
+    }
+
+}
