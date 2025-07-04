2c7f3d2efce9c5b438e01a20e209eec7dbfbfc55
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-04 10:55:00 -0700

    completed exercise 03

--- src/labs_examples/exception_handling/labs/Exercise_03.java
+++ src/labs_examples/exception_handling/labs/Exercise_03.java
@@ -6,3 +6,27 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a try/catch/finally block.
  *
  */
+
+public class Exercise_03 {
+
+    public static void main(String[] args) {
+
+        try {
+            int[] nums = {1, 2, 3};
+            System.out.println(nums[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Index is outside of the array.");
+        }
+
+        try {
+            int[] nums = {1, 2, 3};
+            int quotient = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero.");
+        } finally {
+            System.out.println("End of program.");
+        }
+
+
+    }
+}
