3df95a3a9b69fd95521b36517d2cd6e34cc36e4f
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-04 10:43:26 -0700

    completed exercise 02

--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -5,6 +5,16 @@ package labs_examples.exception_handling.labs;
  *
  *      Demonstrate a try/catch with multiple catch statements.
  *
+ *
+ *try {
+ *     int[] numbers = {1, 2, 3};
+ *     int result = 10 / 0;
+ *     System.out.println(numbers[5]);
+ *} catch (ArithmeticException e) {
+ *     System.out.println("Cannot divide by zero.");
+ *} catch (ArrayIndexOutOfBoundsException e) {
+ *     System.out.println("Array index is out of bounds.")
+ *}
  */
 
 
