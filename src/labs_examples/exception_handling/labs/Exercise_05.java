31e8a5ca39efe4650fe556f07b9bcfe978431c77
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-04 11:47:58 -0700

    completed exercise 5

--- src/labs_examples/exception_handling/labs/Exercise_05.java
+++ src/labs_examples/exception_handling/labs/Exercise_05.java
@@ -13,6 +13,17 @@ class Example {
 
     public static void main(String[] args) {
 
+        try {
+            someMethod(3);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("That index location is out of bounds.");
+        }
+
+    }
+
+    public static int someMethod(int x) throws ArrayIndexOutOfBoundsException {
+        int[] nums = {1, 2};
+        return nums[x];
     }
 
 }
