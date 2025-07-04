db726799d2e2a9d87ed1e97c6565bb4808d4a6d9
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-04 12:33:53 -0700

    completed exercise 6

--- src/labs_examples/exception_handling/labs/Exercise_06.java
+++ src/labs_examples/exception_handling/labs/Exercise_06.java
@@ -7,3 +7,28 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+public class Exercise_06 {
+
+    public static void main(String[] args) {
+
+        try {
+            testMethod1();
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero.");
+        }
+
+        System.out.println("Continuing program.");
+
+    }
+
+    public static void testMethod1() throws ArithmeticException{
+        testMethod2();
+    }
+
+    public static void testMethod2() throws ArithmeticException {
+        int num = 2 / 0;
+        System.out.println("The quotient is: " + num);
+    }
+
+}
+
