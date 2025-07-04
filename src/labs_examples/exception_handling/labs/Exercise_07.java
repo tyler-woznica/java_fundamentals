3c91fe42bac44bdb3de67f2402c48ce1049b3f40
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-04 14:28:40 -0700

    completed exercise 7

--- src/labs_examples/exception_handling/labs/Exercise_07.java
+++ src/labs_examples/exception_handling/labs/Exercise_07.java
@@ -6,3 +6,35 @@ package labs_examples.exception_handling.labs;
  *      1) Create a custom exception.
  *      2) Demonstrate a method throwing your custom exception.
  */
+
+public class Exercise_07 {
+
+    public static void main(String[] args) {
+
+        try {
+            checkNum(0);
+        } catch (CustomException exc) {
+            System.out.println("Caught custom exception: " + exc.getMessage());
+        }
+
+        System.out.println("Continue...");
+
+    }
+
+    public static void checkNum(int x) throws CustomException{
+        if (x <= 0) {
+            throw new CustomException("Number must be greater than zero.");
+        }
+        System.out.println("Number is valid: " + x);
+    }
+
+
+
+    public static class CustomException extends Exception {
+        public CustomException(String message) {
+            super(message);
+        }
+    }
+
+
+}
