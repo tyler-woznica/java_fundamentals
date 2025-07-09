bf6e3223210b918e3b5a8482ea07cb12074792ab
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-08 17:36:21 -0700

    started Exercise_03

--- src/labs_examples/input_output/labs/Exercise_03.java
+++ src/labs_examples/input_output/labs/Exercise_03.java
@@ -1,5 +1,9 @@
 package labs_examples.input_output.labs;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+
 /**
  * Input/Output Exercise 3: variety
  *
@@ -9,3 +13,18 @@ package labs_examples.input_output.labs;
  *    4) Demonstrate the use of the DataInputStream and DataOutputStream
  *
  */
+
+class Exercise_03 {
+    public static void main(String[] args) {
+        String byteFile = "src/labs_examples/input_output/files/byte_data";
+        String charFile = "src/labs_examples/input_output/files/char_data.txt";
+
+        try (
+                FileOutputStream fos = new FileOutputStream(byteFile);
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                DataOutputStream dos = new DataOutputStream(bos);
+                ) {
+            dos.writ
+        }
+    }
+}
