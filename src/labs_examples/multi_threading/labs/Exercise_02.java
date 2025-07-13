48abf0d1c961331b53ace01e1cbeb5d5e9e3ffd3
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-13 14:32:21 -0700

    completed exercise 2

--- src/labs_examples/multi_threading/labs/Exercise_02.java
+++ src/labs_examples/multi_threading/labs/Exercise_02.java
@@ -5,3 +5,19 @@ package labs_examples.multi_threading.labs;
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+class Exercise_02 {
+    public static void main(String[] args) {
+
+        MyThread thread1 = new MyThread();
+        thread1.start();
+
+    }
+
+    public static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Running MyThread that extends Thread.");
+        }
+    }
+}
