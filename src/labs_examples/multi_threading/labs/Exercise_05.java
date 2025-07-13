747acade094b2df0322a81aecfca8e3e9e8ab79a
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-13 16:20:45 -0700

    completed exercise 5

--- src/labs_examples/multi_threading/labs/Exercise_05.java
+++ src/labs_examples/multi_threading/labs/Exercise_05.java
@@ -1,8 +1,35 @@
 package labs_examples.multi_threading.labs;
 
-/**
- * Multithreading Exercise 5:
- *
- *      Demonstrate the use of a wait() and notify()
- */
+// Multithreading Exercise 5:
+// Demonstrate the use of a wait() and notify()
 
+class Exercise_05 {
+    public static void main(String[] args) {
+
+        SharedObj shared = new SharedObj();
+        Thread waiter = new Thread(() -> shared.waitForSignal());
+        Thread notifier = new Thread(() -> shared.sendSignal());
+
+        waiter.start();
+        notifier.start();
+
+    }
+
+    static class SharedObj {
+
+        public synchronized void waitForSignal() {
+            System.out.println("Waiting for signal...");
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("Recieved signal and continuing...");
+        }
+
+        public synchronized void sendSignal() {
+            System.out.println("Sending signal...");
+            notify();
+        }
+    }
+}
