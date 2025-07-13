ba5487ec43a23aa537873c1ea5b2b374dffd319e
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-13 14:25:29 -0700

    completed exercise 1

--- src/labs_examples/multi_threading/labs/Exercise_01.java
+++ src/labs_examples/multi_threading/labs/Exercise_01.java
@@ -12,5 +12,25 @@ package labs_examples.multi_threading.labs;
 class Exercise_01 {
     public static void main(String[] args) {
 
+        MyThread myThread = new MyThread();
+        Thread thread1 = new Thread(myThread);
+        thread1.start();
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Running anonymous thread.");
+            }
+        });
+        thread2.start();
+
     }
+
+    public static class MyThread implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("Running my thread!");
+        }
+    }
+
 }
