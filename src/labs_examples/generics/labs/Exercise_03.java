e7c68510a9cfd5b98f74d75c631c29ea2d244a1b
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-15 21:08:20 -0700

    built 3 of 4 methods for exercise, tested and working

--- src/labs_examples/generics/labs/Exercise_03.java
+++ src/labs_examples/generics/labs/Exercise_03.java
@@ -1,7 +1,6 @@
 package labs_examples.generics.labs;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 /**
  * Generics Exercise 3:
@@ -17,3 +16,49 @@ import java.util.Collection;
  *      4) Write a generic method to find the largest element within the range (begin, end) of a list.
  *
  */
+
+class Exercise_03 {
+    public static void main(String[] args) {
+
+        System.out.println(sumNums(11, 12.62));
+
+        List<String> words = Arrays.asList("level", "test", "radar", "java", "madam");
+        System.out.println(countPalindromes(words));
+
+        Integer[] numbers = {1, 2, 3, 4};
+        swapElements(numbers, 0,2);
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+    public static <T extends Number, U extends Number> double sumNums(T a, U b) {
+
+        return a.doubleValue() + b.doubleValue();
+
+    }
+
+    public static int countPalindromes(Collection<String> collection) {
+
+        int count = 0;
+        for (String s : collection) {
+            if (s.equals(new StringBuilder(s).reverse().toString())) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+    public static <T> void swapElements(T[] array, int i, int j) {
+
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+
+    //public static <T extends Comparable<T>> T maxInRange(List<T> list, int begin, int end) {
+      //  T max =
+    //}
+
+}
