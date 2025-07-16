243818b19f7c10edbe0664ca594e96aa4729fac6
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-15 20:18:01 -0700

    completed exercise 1

--- src/labs_examples/generics/labs/Exercise_01.java
+++ src/labs_examples/generics/labs/Exercise_01.java
@@ -8,3 +8,48 @@ package labs_examples.generics.labs;
  *          dynamic usage.
  */
 
+class Exercise_01 {
+    public static void main(String[] args) {
+
+        Box<Integer> intBox = new Box<>(100, "Integer Box");
+        System.out.println(intBox);
+
+        Box<String> strBox = new Box<>("Hello Box", "String Box");
+        System.out.println(strBox);
+
+    }
+
+    static class Box<T> {
+        private T item;
+        private String label;
+
+        public Box(T item, String label) {
+            this.item = item;
+            this.label = label;
+        }
+
+        public T getItem() {
+            return item;
+        }
+
+        public void setItem(T item) {
+            this.item = item;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+
+        public void setLabel(String label) {
+            this.label = label;
+        }
+
+        @Override
+        public String toString() {
+            return "Box{" +
+                    "item=" + item +
+                    ", label='" + label + '\'' +
+                    '}';
+        }
+    }
+}
