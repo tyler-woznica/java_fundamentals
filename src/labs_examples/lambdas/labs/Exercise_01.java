b4b14b09356145b737e23d3a6a83221eb9613134
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-21 10:52:31 -0700

    completed exercise 1

--- src/labs_examples/lambdas/labs/Exercise_01.java
+++ src/labs_examples/lambdas/labs/Exercise_01.java
@@ -15,6 +15,8 @@ package labs_examples.lambdas.labs;
        7) Demonstrate the use of at least two built-in functional interfaces from the java.util.function package. */
 
 import java.sql.SQLOutput;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 class Exercise_01 {
 
@@ -47,7 +49,36 @@ class Exercise_01 {
 
         greetAnon.exc();
 
-        OneParam<String>
+        OneParam<String> upperLambda = s -> s.toUpperCase();
+        System.out.println(upperLambda.process("hello was lowercase"));
+
+        OneParam<String> upperAnon = new OneParam<String>() {
+            @Override
+            public String process(String input) {
+                return input.toUpperCase();
+            }
+        };
+
+        System.out.println(upperAnon.process("world was lowercase"));
+
+        TwoParam<Integer, Integer, Integer> addLambda = (a, b) -> a + b;
+        System.out.println("Sum (lambda): " + addLambda.compute(2, 4));
+
+        TwoParam<Integer, Integer, Integer> addAnon = new TwoParam<Integer, Integer, Integer>() {
+            @Override
+            public Integer compute(Integer a, Integer b) {
+                return a + b;
+            }
+        };
+
+        System.out.println("Sum (anon class): " + addAnon.compute(3, 6));
+
+        Function<String, Integer> lengthFunction = str -> str.length();
+        System.out.println("Length of 'Lambda': " + lengthFunction.apply("Lambda"));
+
+        BiFunction<String, String, String> concatFunction = (s1, s2) -> s1 + s2;
+        System.out.println("Concat result: " + concatFunction.apply("Hello, ", "World!"));
+
     }
 
 
