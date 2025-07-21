f655babec351a3f92c2ee95a30840d48cb7c5114
Tyler Woznica <tylerwoznica@outlook.com>
2025-07-21 16:50:41 -0700

    completed exercise 3

--- src/labs_examples/lambdas/labs/Exercise_03.java
+++ src/labs_examples/lambdas/labs/Exercise_03.java
@@ -1,5 +1,9 @@
 package labs_examples.lambdas.labs;
 
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 /**
  * Lambdas Exercise 3:
  *
@@ -8,3 +12,31 @@ package labs_examples.lambdas.labs;
  *      3) Demonstrate the use of a constructor reference
  *
  */
+
+class Exercise_03 {
+    public static void main(String[] args) {
+
+        Function<String, Integer> staticReference = Integer::parseInt;
+        int num = staticReference.apply("123");
+        System.out.println("Parsed int from static method: " + num);
+
+        String string = "hello world";
+        Supplier<String> instanceReference = string::toUpperCase;
+        String upper = instanceReference.get();
+        System.out.println("Uppercase string from instance method: " + upper);
+
+        Supplier<MyClass> constructorReference = MyClass::new;
+        MyClass myObj = constructorReference.get();
+        System.out.println("New object from constructor reference: " + myObj);
+
+
+    }
+
+    static class MyClass {
+        @Override
+        public String toString() {
+            return "MyClass instance";
+        }
+    }
+
+}
