84b6420372dc5f31b50fb91e523a5c4ec1ef4f4c
Tyler Woznica <tylerwoznica@outlook.com>
2025-08-21 11:03:38 -0700

    completed steps 1-3 of exercise 1

--- src/labs_examples/datastructures/linkedlist/labs/Exercise_01.java
+++ src/labs_examples/datastructures/linkedlist/labs/Exercise_01.java
@@ -1,26 +1,61 @@
 package labs_examples.datastructures.linkedlist.labs;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
-/**
- *      LinkedLists - Exercise_01
- *
- *      Demonstrate your mastery of Java's built-in LinkedList class by demonstrating the following:
- *
- *
- *      1) create a LinkedList (from Java's libraries)
- *      2) add()
- *      3) addAll()
- *      4) addFirst()
- *      5) addLast()
- *      6) getFirst()
- *      7) getLast()
- *      8) get()
- *      9) set()
- *      10) push()
- *      11) pop()
- *      12) remove()
- *      13) contains()
- *      14) listIterator()
- *      15) clear()
- */
+public class Exercise_01 {
 
+    // 1) create LinkedList
+    static LinkedList<Integer> myLinkedList = new LinkedList<>();
+
+    public static void main(String[] args) {
+
+        // Create collection for 3)
+        List<Integer> listOne = Arrays.asList(2, 3);
+
+        // 2) add()
+        myLinkedList.add(1);
+        viewLine();
+
+        // 3) addAll()
+        try {
+            myLinkedList.addAll(1, listOne);
+            viewLine();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        // 4) addFirst()
+
+        // 5) addLast()
+
+        // 6) getFirst()
+
+        // 7) getLast()
+
+        // 8) get()
+
+        // 9) set()
+
+        // 10) push()
+
+        // 11) pop()
+
+        // 12) remove()
+
+        // 13) contains()
+
+        // 14) listIterator()
+
+        // 15) clear()
+
+    }
+
+    public static void viewLine () {
+        System.out.println("** Updated values **");
+        myLinkedList.forEach(System.out::println);
+        System.out.println();
+    }
+
+}
 
